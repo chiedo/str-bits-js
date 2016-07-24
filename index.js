52b047a5586d134c5519f37e0dfa9cd3c2450c5b
Chiedo <chiedo@chie.do>
2016-07-24 18:44:29 -0400

    Fixed zero pad error

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-module.exports =  function(str) {
+zeroPad = function(num) {
+  return "00000000".slice(String(num).length) + num
+}
+
+strBits =  function(str) {
   var output = '';
 
   for (i=0; i < str.length; i++) {
-    output += str[i].charCodeAt(0).toString(2);
+    output += zeroPad(str[i].charCodeAt(0).toString(2));
   }
 
   return output;
 }
+
+module.exports = strBits;
